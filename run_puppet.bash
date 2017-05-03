38ca984cf309028da226ccf74e109e350d41586b
Alan Rickman <alan@Alans-MBP.home>
2017-05-03 07:24:57 +0100

    Run r10k and copy local modules before running Puppet

--- run_puppet.bash
+++ run_puppet.bash
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# Install modules from Puppetfile
+cd /tmp/vagrant-puppet/environments/production && sudo r10k puppetfile install --verbose
+
+# Copy local dev modules
+sudo cp -R /home/vagrant/$(hostname)/local/modules/* /tmp/vagrant-puppet/environments/production/modules/
+
+# Run Puppet
 sudo /opt/puppetlabs/bin/puppet apply --hiera_config=/tmp/vagrant-puppet/hiera.yaml --environmentpath /tmp/vagrant-puppet/environments/ --environment production /tmp/vagrant-puppet/environments/production/manifests
